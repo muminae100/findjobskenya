754f1431a4af63aeb4de3f67510dddb839b0c956
muminae100 <smuminaetx100@gmail.com>
2022-03-19 20:10:55 +0300

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -8,7 +9,11 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+uri = os.getenv("SQLALCHEMY_DATABASE_URI")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
